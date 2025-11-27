98ab9b6a6210ee788345672a9d1150ffd1ea5a3a
Talles <worktalles@hotmail.com>
2025-11-27 20:17:04 -0300

    chore: use real data

--- web/src/app/(app)/communities/page.tsx
+++ web/src/app/(app)/communities/page.tsx
@@ -1,33 +1,9 @@
+import { getCommunities } from "@/http/communities/get-communities"
 import { CommunitiesTable } from "./communities-table"
 
-const mockCommunities = [
-  {
-    id: 12,
-    size: 48,
-    total_tx: 932,
-    high_fanin_pct: 37,
-    avg_risk: 61,
-    status: "Suspeita",
-  },
-  {
-    id: 5,
-    size: 12,
-    total_tx: 110,
-    high_fanin_pct: 5,
-    avg_risk: 12,
-    status: "Normal",
-  },
-  {
-    id: 7,
-    size: 102,
-    total_tx: 5200,
-    high_fanin_pct: 64,
-    avg_risk: 92,
-    status: "Crítica",
-  },
-]
-
-export default function CommunitiesPage() {
+export default async function CommunitiesPage() {
+  const communities = await getCommunities()
+  
   return (
     <div className="space-y-4 p-4">
       <div>
@@ -37,7 +13,7 @@ export default function CommunitiesPage() {
         </p>
       </div>
 
-      <CommunitiesTable data={mockCommunities} />
+      <CommunitiesTable data={communities} />
     </div>
   )
 }
