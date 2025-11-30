5c7ce3488b2f7dfb7ba6b9376532babf7f08370c
Talles <worktalles@hotmail.com>
2025-11-29 21:38:24 -0300

    chore: get risk transactions

--- web/src/app/(app)/dashboard/page.tsx
+++ web/src/app/(app)/dashboard/page.tsx
@@ -5,13 +5,15 @@ import { SectionCards } from "@/components/section-cards"
 
 
 import data from "./data.json"
+import { getRiskTransactions } from "@/http/metrics/get-risk-transactions"
 
-export default function Page() {
+export default async function Page() {
+  const riskTransactions = await getRiskTransactions()
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <SectionCards />
       <div className="px-4 lg:px-6">
-        <RiskTransactionsGraph />
+        <RiskTransactionsGraph data={riskTransactions} />
       </div>
       
       <DataTable data={data} />
