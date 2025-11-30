406abcfafbcb89f763be8d4f5ac862ec6be3892b
Talles <worktalles@hotmail.com>
2025-11-29 21:36:30 -0300

    chore: use data on cards

--- web/src/components/section-cards.tsx
+++ web/src/components/section-cards.tsx
@@ -1,50 +1,43 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
-
-import { Badge } from "@/components/ui/badge"
-import {
-  Card,
-  CardAction,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { RiskMetricCard } from "@/app/(app)/dashboard/risk-card"
 import { Activity, ShieldAlert } from "lucide-react"
+import { RiskMetricCard } from "@/app/(app)/dashboard/risk-card"
+import { getMetricsOverview } from "@/http/metrics/get-overview"
 
-export function SectionCards() {
+export async function SectionCards() {
+  const data = await  getMetricsOverview()
+
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+
       <RiskMetricCard
         title="Transações Hoje"
-        value="1.250"
+        value={data.transacoes_hoje}
         subtitle="Fluxo total do dia"
-        trend="+12.5%"
+        trend="+12%"
         trendPositive
         icon={<Activity className="w-4 h-4" />}
       />
 
       <RiskMetricCard
         title="Suspeitas"
-        value="87"
+        value={data.suspeitas_hoje}
         subtitle="Detectadas nas últimas 24h"
-        trend="+8%"
+        trend="-3%"
         trendPositive={false}
         icon={<ShieldAlert className="w-4 h-4" />}
       />
 
       <RiskMetricCard
         title="Fraudes Confirmadas"
-        value="14"
-        subtitle="Casos validados pelo time"
-        trend="+3%"
+        value={data.fraudes_confirmadas}
+        subtitle="Casos validados"
+        trend="+2%"
         trendPositive
       />
 
       <RiskMetricCard
         title="Score Médio"
-        value="42.5"
-        subtitle="Últimas 24 horas"
+        value={data.avg_score_24h ?? 0}
+        subtitle="Últimas 24h"
       />
     </div>
   )
