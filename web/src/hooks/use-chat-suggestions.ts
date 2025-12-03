02d27d76d6fdaa02e330693c5074677574729b29
Talles <worktalles@hotmail.com>
2025-12-03 00:05:46 -0300

    chore: create hook chat suggestions

---
+++ web/src/hooks/use-chat-suggestions.ts
@@ -0,0 +1,17 @@
+// /hooks/use-chat-suggestions.ts
+
+export function useChatSuggestions() {
+  function random(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const suggestions = [
+    `Exibir resumo da conta ${random(1, 500)}`,
+    `Ver anomalias detectadas na conta ${random(1, 500)}`,
+    `Consultar risco da conta ${random(1, 500)}`,
+    `Listar transações da conta ${random(1, 500)}`,
+    `Ver detalhes da transação ${random(100, 5000)}`,
+  ];
+
+  return suggestions;
+}
